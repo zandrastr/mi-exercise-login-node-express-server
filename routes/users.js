561f80d33dac689ae246cc180d3b589688df1ebd
Sandra S <zandrastr@hotmail.com>
2023-03-06 17:28:41 +0100

    added user data in route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+let users = [
+  {id:1, username: "Simon", password: "12345"},
+  {id:2, username: "Anna", password: "11111"},
+  {id:3, username: "Camilla", password: "00000"},
+  {id:4, username: "Josef", password: "myPassword"}
+];
+
+/* GET users listing */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json(users);
 });
 
 module.exports = router;
